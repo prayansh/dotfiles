cb94e572f3c53c8b6546e4ce1413ddebf66b1639
Prayansh Srivastava <prayansh@gmail.com>
2018-05-27 14:34:36 +0530

    remove unwanted files

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,40 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
-
-git pull origin master;
-
-function doIt() {
-	rsync --exclude ".git/" \
+# Copy dotfiles
+rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
-		--exclude ".osx" \
-		--exclude "bootstrap.sh" \
+		--exclude ".macos" \
+    --exclude "brew.sh"
+    --exclude "setup.sh"
 		--exclude "README.md" \
-		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
-}
+source brew.sh
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
-	fi;
-fi;
-unset doIt;
+# Setup ZSH
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+cp ~/dotfiles/.zshrc ~/.zshrc
+cp ~/dotfiles/.gitconfig ~/.gitconfig
+cp ~/dotfiles/.gitignore ~/.gitignore
+cp ~/dotfiles/.vimrc ~/.vimrc
+cp ~/dotfiles/env.sh ~/env.sh
+
+
+# Install ZSH plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install fonts
+git clone https://github.com/powerline/fonts.git
+cd fonts
+./install.sh
+cd ..
+rm -rf fonts
+
+# Install powerlevel9k
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Install vtop
+npm install -g vtop
--- brew.sh
+++ brew.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Install command-line tools using Homebrew.
+xcode-select --install
 
 # Make sure we’re using the latest Homebrew.
 brew update
@@ -18,17 +19,6 @@ brew install moreutils
 brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
 brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew install bash-completion2
-
-# Switch to using brew-installed bash as default shell
-if ! fgrep -q '/usr/local/bin/bash' /etc/shells; then
-  echo '/usr/local/bin/bash' | sudo tee -a /etc/shells;
-  chsh -s /usr/local/bin/bash;
-fi;
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -77,6 +67,7 @@ brew install xz
 
 # Install other useful binaries.
 brew install ack
+brew install node
 #brew install exiv2
 brew install git
 brew install git-lfs
@@ -92,6 +83,9 @@ brew install ssh-copy-id
 brew install tree
 brew install vbindiff
 brew install zopfli
+
+# install iterm2
+brew cask install iterm2
 
 # Remove outdated versions from the cellar.
 brew cleanup
