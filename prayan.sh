55e4bf3d300e88afb54443c5ad6ee8846aead652
Prayansh Srivastava <prayansh@gmail.com>
2018-05-27 14:51:54 +0530

    fix syntax error in prayan.sh

--- prayan.sh
+++ prayan.sh
@@ -8,7 +8,6 @@ if [ "$1" == "--update" -o "$1" == "-u" ]; then
   TIMESTAMP=`date "+%Y, %m-%d"`
   git commit -m "updated settings on $TIMESTAMP"
   unset TIMESTAMP
-fi
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure you want to continue? (y/n) " -n 1;
 	echo "";
