8d5b87aa65c31a4796caaa7916384f8973dc9360
Prayansh Srivastava <prayansh@gmail.com>
2019-05-14 00:46:06 -0700

    update script to add tmux config

--- prayan.sh
+++ prayan.sh
@@ -5,8 +5,10 @@ if [ "$1" == "--update" -o "$1" == "-u" ]; then
   cp ~/.gitignore .gitignore
   cp ~/.vimrc .vimrc
   cp ~/env.sh env.sh
+  cp ~/.tmux.conf .tmux.conf
   TIMESTAMP=`date "+%Y, %m-%d"`
-  git commit -m "updated settings on $TIMESTAMP"
+  #git add .zshrc .gitconfig .gitignore .vimrc env.sh
+  git commit -am "updated settings on $TIMESTAMP"
   unset TIMESTAMP
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure you want to continue? (y/n) " -n 1;
